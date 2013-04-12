26410dbe84ea883d166e22a81714934da3b8683b
maltindal <muhammet@altindal.de>
2013-04-12 22:08:35 +0200

    Adding tests for onTransenlightenment call on data change

---
+++ test/test_react.rb
@@ -0,0 +1,28 @@
+require 'test/unit'
+require 'conjoiners'
+
+class TestReact < Test::Unit::TestCase
+
+  class Test
+    attr_accessor :test_value
+    attr_reader :result
+    def onTransenlightenment
+      @result = @test_value.to_i + 1
+    end
+  end
+
+  def setup
+    @cj1 = Test.new
+    @cj2 = Test.new
+    Conjoiners::implant(@cj1, "./conf_react.json", "test")
+    Conjoiners::implant(@cj2, "./conf_react.json", "test2")
+  end
+
+  def test_send
+    sleep 1
+    @cj1.test_value = 1
+    sleep 1
+    assert_equal(2, @cj2.result)
+  end
+
+end
